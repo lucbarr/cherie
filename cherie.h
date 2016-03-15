da76486afc4d8a08f5160cbac9fb9831ded31581
Luciano Barreira <meckmao@gmail.com>
2016-03-15 18:15:32 -0300

    Added design, defined loci and consts

---
+++ cherie.h
@@ -0,0 +1,12 @@
+#ifndef CHERIE_H
+#define CHERIE_H 
+
+const int FedThresold = 4;
+const int ThirstThreshold = 5;
+const int TiredThreshold = 6;
+const int FullBladder = 4;
+const float ScratchChance = 0.90f;
+
+class Cherie;
+
+#endif
---
+++ loci.h
@@ -0,0 +1,12 @@
+#ifndef LOCI_H
+#define LOCI_H
+
+enum loci {
+	living_room,
+	annas_room,
+	dining_room,
+	bowls_room
+};
+
+
+#endif
